63095eaeb7fd947f25b4bede74e2a62bfbca8698
keinnerross <keinnerross@gmail.com>
2022-10-30 20:25:52 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import herosRoutes from "./routes/heros.routes.js";
 import appRoutes from "./routes/app.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(appRoutes);
 app.use("/api", herosRoutes);
 
